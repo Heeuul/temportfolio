116276809d3e667b760d9b4aebf26a1e3bf7c8b8
Heeuul <46469679+Heeuul@users.noreply.github.com>
2021-12-12 20:31:08 +0800

    v0.5

--- pf.py
+++ pf.py
@@ -3,10 +3,13 @@ import streamlit as st
 st.title("Game Academic Projects:") 
 
 st.header("VR Healthcare System for Patients with Trypanophobia") 
+st.markdown("_Final Year Project (Jul 2020 ~ Mar 2021)_") 
 st.subheader("Virtual reality game which combats needle fear among pediatric patients") 
 
 st.header("MagnifiCast") 
+st.markdown("_Game Production (Nov 2020 ~ Mar 2021)_") 
 st.subheader("Tower defense mobile game built from competitive analysis of mechanically similar games on the market") 
 
 st.header("Violet World") 
+st.markdown("_Computer Graphic Fundamentals (Nov 2019 ~ Mar 2020)_") 
 st.subheader("Imaginative 3D world of outer space fully built with program codes") 
