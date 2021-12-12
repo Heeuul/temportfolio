bcc63492a005070d63e05af74d77f20c594e4ee1
Heeuul <46469679+Heeuul@users.noreply.github.com>
2021-12-12 20:26:03 +0800

    v0.4

--- pf.py
+++ pf.py
@@ -1,4 +1,12 @@
 import streamlit as st 
 
-st.title("Aiman Hans Bin Husin") 
-st.header("Game Academic Projects:") 
+st.title("Game Academic Projects:") 
+
+st.header("VR Healthcare System for Patients with Trypanophobia") 
+st.subheader("Virtual reality game which combats needle fear among pediatric patients") 
+
+st.header("MagnifiCast") 
+st.subheader("Tower defense mobile game built from competitive analysis of mechanically similar games on the market") 
+
+st.header("Violet World") 
+st.subheader("Imaginative 3D world of outer space fully built with program codes") 
